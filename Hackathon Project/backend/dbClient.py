027b60d5b2d5993b8f81bdc1af6193619b8477c7
kiryll bahutski <kb01696@surrey.ac.uk>
2026-03-20 23:39:28 +0000

    fixed key leak

--- Hackathon Project/backend/dbClient.py
+++ Hackathon Project/backend/dbClient.py
@@ -1,7 +1,10 @@
 from supabase import create_client, Client
+import os
+from dotenv import load_dotenv
 
+load_dotenv()
 
-url = "https://dupuljrdyiznjexlirud.supabase.co"
-key = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImR1cHVsanJkeWl6bmpleGxpcnVkIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzMzMTA5NTksImV4cCI6MjA4ODg4Njk1OX0.5E6EIkgyISkwST7zcBJP89MZmkm2B9DqpvpYIK2GaJg"
+url = os.getenv("SUPABASE_URL")
+key = os.getenv("SUPABASE_ANON_KEY")
 
 supabase: Client = create_client(url, key)
